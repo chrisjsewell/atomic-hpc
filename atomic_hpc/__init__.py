ff727cbd0148d2bf8c6028413fd5998140b69648
Chris Sewell <chrisj_sewell@hotmail.com>
2017-09-05 17:17:08 +0100

    python 2.7 fix

--- atomic_hpc/__init__.py
+++ atomic_hpc/__init__.py
@@ -1,5 +1,12 @@
+import logging
+from atomic_hpc import utils
+try:
+    utils.add_loglevel("EXEC", logging.INFO + 1)
+except AttributeError:
+    pass
+logger = logging.getLogger(__name__)
 
-from atomic_hpc import config_yaml, deploy_runs, mockssh, context_folder, utils
+from atomic_hpc import config_yaml, deploy_runs, mockssh, context_folder
 
 __version__ = "0.1.4"
 
